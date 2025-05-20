4e9f912bfe678aba2c86af8937bb6d60b78b986e
amirabayoumi <149677044+amirabayoumi@users.noreply.github.com>
2025-05-20 14:31:25 +0200

    add api token

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,27 @@ Route::get('/user', function (Request $request) {
 // Event routes
 Route::apiResource('events', EventController::class);
 Route::apiResource('categories', CategoryController::class);
+
+
+//api/token Auth , to be used to see any api (sanctom token)
+
+
+// Route::post('/token', function (Request $request) {
+//     $request->validate([
+//         'email' => 'required|email',
+//         'password' => 'required',
+//     ]);
+
+//     if (Auth::attempt($request->only('email', 'password'))) {
+//         $user = Auth::user();
+//         $token = $user->createToken('API Token')->plainTextToken;
+
+//         return response()->json(['token' => $token]);
+//     }
+
+//     return response()->json(['error' => 'Unauthorized'], 401);  
+
+// });
+
+
+
